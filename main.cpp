555e37275f8135feca60e7744cfd9ca823a7476e
Rajesh Mandava <rajesh.mandava07@gmail.com>
2022-04-14 20:24:23 +0000

    init commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <memory>
 
+class myClass{
+
+  public:
+  void print()
+  {
+    std::cout<<"print myClass"<<std::endl;
+  }
+
+};
+
+void usemyClass()
+{
+  std::shared_ptr<myClass> myClassSPtr = std::make_shared<myClass>();
+  myClassSPtr->print();
+}
 
 void print(int i)
 {
@@ -12,5 +28,6 @@ int main()
 {
   std::cout<<"This is test C++ container"<<std::endl;
   print(4);
+  usemyClass();
   return 0;
 }
